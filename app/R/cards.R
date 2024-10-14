507f5c3393d83ccaa236c7d5d2194f11ba6530f9
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-10-14 22:12:32 +0100

    Add poker-hand recognition

--- app/R/cards.R
+++ app/R/cards.R
@@ -24,7 +24,7 @@ get_card_name <- function(file) {
     gsub("_", " ", x = _)
 }
 
-get_card_score <- function(hand) {
+score_hand <- function(hand) {
 
   if (length(hand) == 0) return(0)
 
@@ -44,5 +44,62 @@ get_card_score <- function(hand) {
   ) |>
     unlist() |>
     sum()
+
+}
+
+evaluate_poker_hand <- function(hand) {
+
+  if (length(hand) == 0) return("no hand")
+
+  value_table <- substr(hand, 1, 1) |> table() |> sort()
+  suit_table  <- substr(hand, 2, 2) |> table() |> sort()
+
+  values_ordered <- enumerate_cards(hand) |> sort()
+  is_straight <- all(unique(diff(values_ordered)) == 1)
+
+  if (
+    length(hand) == 5 &
+    all(names(value_table) %in% c("A", "K", "Q", "J", "T")) &
+    is_straight &
+    length(suit_table) == 1
+  ) {
+    return("royal flush")
+  }
+  if (length(hand) == 5 & is_straight & length(suit_table) == 1) return("straight flush")
+  if (max(value_table == 4)) return("four of a kind")
+  if (
+    length(hand) == 5 &
+    length(which(value_table == 2)) == 1 &
+    length(which(value_table == 3)) == 1
+  ) {
+    return("full house")
+  }
+  if (length(hand) == 5 & length(suit_table) == 1) return("flush")
+  if (length(hand) == 5 & is_straight) return("straight")
+  if (!is.na(table(value_table)["3"]) && table(value_table)["3"] == 1) return("three of a kind")
+  if (length(hand) > 3 & length(which(value_table == 2)) == 2) return("two pair")
+  if (!is.na(table(value_table)["2"]) && table(value_table)["2"] == 1) return("a pair")
+  if (length(hand) == length(value_table)) return("high card")
+
+}
+
+enumerate_cards <- function(hand) {
+
+  values <- substr(hand, 1, 1)
+
+  lapply(
+    values,
+    \(value) switch(
+      value,
+      "A" = "14",
+      "K" = "13",
+      "Q" = "12",
+      "J" = "11",
+      "T" = "10",
+      value
+    )
+  ) |>
+    unlist() |>
+    as.numeric()
 
 }
